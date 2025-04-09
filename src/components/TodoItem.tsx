d46125f14a2a0629ffd94ecff434b51e0307231c
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-04-09 18:29:44 +0300

    feat: Add styles to TodoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -18,30 +18,30 @@ const TodoItem: React.FC<TodoItemProps> = ({
   onDelete,
 }) => {
   return (
-    <li className={`todo-item ${completed ? 'completed' : ''}`}>
-      <span
-        onClick={() => onToggle(id)}
-        className="task-text"
-      >
-        {text}
-      </span>
+    <li className="todo-item">
+      <div className="checkbox-wrapper">
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={() => onToggle(id)}
+        />
+        <span
+          className={`todo-item__text ${completed ? 'completed' : ''}`}
+          onClick={() => onToggle(id)}
+        >
+          {text}
+        </span>
+      </div>
       <button
-        onClick={() => onDelete(id)}
         className="delete-button"
+        onClick={() => onDelete(id)}
       >
+        <div className="delete-button__bg"></div>
         <svg
-          width="800px"
-          height="800px"
           viewBox="0 0 24 24"
-          fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
-            fill="#0F1729"
-          />
+          <path d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" />
         </svg>
       </button>
     </li>
