e99eda16c959c7498b6ba620392789a93bfcb16c
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-04-09 19:09:04 +0300

    feat: Add theme toggling logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from "react";
-import Todo from "./components/Todo";
-import "./App.scss";
+import React, { useState } from 'react';
+import Todo from './components/Todo';
+import './App.scss';
 
 const App: React.FC = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+
+    setTheme(newTheme);
+
+    document.documentElement.setAttribute('data-theme', newTheme);
+  };
+
   return (
-    <div className='App'>
-      <Todo />
+    <div
+      className="App"
+      data-theme={theme}
+    >
+      <Todo onThemeToggle={toggleTheme} />
     </div>
   );
 };
