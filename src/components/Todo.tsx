9476eb4225025efaf82ef395dd49353835c434be
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-04-09 17:06:47 +0300

    feat: Add basic logic

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,8 @@
-import React, { useState, FormEvent } from "react";
-import ThemeToggle from "./ThemeToggle";
-import "./Todo.scss";
+import React, { useState, FormEvent } from 'react';
+import ThemeToggle from './ThemeToggle';
+import TodoItem from './TodoItem';
+
+import './Todo.scss';
 
 interface Task {
   id: number;
@@ -9,35 +11,74 @@ interface Task {
 }
 
 const Todo: React.FC = () => {
-  const [inputValue, setInputValue] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>('');
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Здесь добавить логику добавления задачи
-    setInputValue("");
+
+    if (inputValue.trim() === '') return;
+
+    const newTask: Task = {
+      id: Date.now(),
+      text: inputValue,
+      completed: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setInputValue('');
+  };
+
+  const toggleTaskCompletion = (id: number) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task,
+      ),
+    );
+  };
+
+  const deleteTask = (id: number) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
   };
 
   return (
-    <div className='todo-container'>
-      <div className='todo-header'>
+    <div className="todo-container">
+      <div className="todo-header">
         <h1>Список задач</h1>
         <ThemeToggle />
       </div>
 
-      <form className='input-section' onSubmit={handleSubmit}>
+      <form
+        className="input-section"
+        onSubmit={handleSubmit}
+      >
         <input
-          type='text'
-          className='task-input'
-          placeholder='Введите новую задачу'
+          type="text"
+          className="task-input"
+          placeholder="Введите новую задачу"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <button type='submit' className='add-button'>
+        <button
+          type="submit"
+          className="add-button"
+        >
           Добавить
         </button>
       </form>
 
-      <div className='tasks-list'>{/* Здесь должны отображаться задачи */}</div>
+      <ul className="tasks-list">
+        {tasks.map((task) => (
+          <TodoItem
+            key={task.id}
+            id={task.id}
+            text={task.text}
+            completed={task.completed}
+            onToggle={toggleTaskCompletion}
+            onDelete={deleteTask}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
